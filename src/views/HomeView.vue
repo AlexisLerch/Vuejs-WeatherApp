e18a94a58b33090addae30eece4fce5a30b2e248
Alexis Lerch <alexis.lerch123@gmail.com>
2024-08-15 10:11:07 -0300

    HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
-  <main class="container">
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" v-model="searchQuery" placeholder="Search for a city or state" class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+    </div>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const searchQuery = ref("");
+</script>
