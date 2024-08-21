86d6ea59519a88f27159114b78f55f536b6d8fe7
Alexis Lerch <alexis.lerch123@gmail.com>
2024-08-21 10:34:46 -0300

    Agregando para mostrar los errores en pantalla

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,8 @@
       <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search for a city or state" class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" />
       <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
       v-if="mapboxSearchResults">
+      <p v-if="searchError">Sorry, something went wrong, please try again.</p>
+      <p v-if="!serverError && mapboxSearchResults.length === 0">No results match your query, try a different term.</p>
         <li v-for="searchResult in mapboxSearchResults" key="searchResult.id" class="py-2 cursor-pointer">
           {{ searchResult.place_name }}
         </li>
@@ -20,6 +22,7 @@ const mapboxAPIKey = "pk.eyJ1IjoiYWxleGlzbGVyY2giLCJhIjoiY2x6dmNoMWdkMDYyazJpcHZ
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
